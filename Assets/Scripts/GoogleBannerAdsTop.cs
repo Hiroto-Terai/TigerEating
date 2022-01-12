78e3a9f1d8d104c85067ef277a22f8e10a3eb1b4
Hiroto-Terai <hiro08051610@gmail.com>
2022-01-13 01:16:56 +0900

    広告を実装(エラー発生中)

---
+++ Assets/Scripts/GoogleBannerAdsTop.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using GoogleMobileAds.Api;
+
+public class GoogleBannerAdsTop : MonoBehaviour
+{
+  private BannerView bannerView;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    // Google Mobile Ads SDKの初期化
+    MobileAds.Initialize(initStatus => { });
+
+    this.RequestBanner();
+    SceneManager.activeSceneChanged += OnActiveSceneChanged;
+  }
+
+  void OnActiveSceneChanged(Scene prevScene, Scene nextScene)
+  {
+    this.bannerView.Destroy();
+  }
+  private void RequestBanner()
+  {
+#if UNITY_IPHONE
+    string adUnitId = "ca-app-pub-1161341815062676/1471699507";
+
+#else
+    string adUnitId = "unexpected_platform";
+#endif
+
+    // Create a 320x50 banner at the top of the screen.
+    this.bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+    this.bannerView.OnAdLoaded += this.HandleOnAdLoaded;
+
+    // Create an empty ad request.
+    AdRequest request = new AdRequest.Builder().Build();
+
+    // Load the banner with the request.
+    this.bannerView.LoadAd(request);
+  }
+  public void HandleOnAdLoaded(object sender, EventArgs args)
+  {
+    MonoBehaviour.print("HandleAdLoaded event received");
+  }
+}
---
+++ Assets/Scripts/GoogleInterStitialAds.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class GoogleInterStitialAds : MonoBehaviour
+{
+  private void Start()
+  {
+    Debug.Log("OnEnable now");
+    RequestInterstitial();
+  }
+  private InterstitialAd interstitial;
+
+  private void RequestInterstitial()
+  {
+#if UNITY_IPHONE
+    string adUnitId = "ca-app-pub-1161341815062676/5371416060";
+#else
+    string adUnitId = "unexpected_platform";
+#endif
+
+    // Initialize an InterstitialAd.
+    this.interstitial = new InterstitialAd(adUnitId);
+    interstitial.OnAdClosed += HandleOnAdClosed;
+
+    // Create an empty ad request.
+    AdRequest request = new AdRequest.Builder().Build();
+    Debug.Log("Build now");
+    // Load the interstitial with the request.
+    this.interstitial.LoadAd(request);
+    Debug.Log("LoadAd now");
+  }
+  // public void OnGUI()
+  // {
+  //   //ShowAds();
+  // }
+  public void ShowAds()
+  {
+    if (this.interstitial.IsLoaded())
+    {
+      Debug.Log("Show now");
+      this.interstitial.Show();
+    }
+  }
+  public void HandleOnAdClosed(object sender, EventArgs args)
+  {
+    Debug.Log("Destroy now");
+    interstitial.Destroy();
+    Time.timeScale = 1;
+  }
+}
